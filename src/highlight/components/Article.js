8b92cf192e869e86d935ed86df7660ab2e590099
yana <bezotechestvo.yana@gmail.com>
2024-10-31 18:31:29 +0300

    fixed

--- src/highlight/components/Article.js
+++ src/highlight/components/Article.js
@@ -5,7 +5,7 @@ function Article(props) {
 
     return (
         <div className="item item-article">
-            <h3><a href="">{title}</a></h3>
+            <h3><a href=".">{title}</a></h3>
             <p className="views">Прочтений: {views}</p>
         </div>
     )
